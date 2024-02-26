c9c28471044fdd99ea080c3a3c3b32d08a569baf
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-02-26 18:07:45 -0300

    Recursividade

--- taurusgym/src/components/navigation-bar/index.js
+++ taurusgym/src/components/navigation-bar/index.js
@@ -1,23 +1,36 @@
-import React from 'react';
+// NavigationBar.js
+
+import React, { useState } from 'react';
 import './styles.css';
 
+const NavigationBar = ({ onPressCallback }) => {
+  const [showMenu, setShowMenu] = useState(false);
 
-const NavigationBar = ({onPressCallback}) => {
+  const handleOptionClick = (category) => {
+    onPressCallback(category);
+    setShowMenu(false); // Fecha o menu após uma opção ser escolhida
+  };
+
   return (
-    <div className ='container-nav-bar'>
+    <div className='container-nav-bar'>
 
       <div className='container-logo'>
-        <img  onClick={() => onPressCallback('')} id='logo' src="/logo01.png" alt="logo" />
+        <img onClick={() => onPressCallback('')} id='logo' src="/logo01.png" alt="logo" />
       </div>
 
       <div className='options'>
-        <ul className='menu'>
-          <li><button className='btn-nav' onClick={() => onPressCallback('OMBRO')}>OMBRO</button></li>
-          <li><button className='btn-nav' onClick={() => onPressCallback('PEITO')}>PEITO</button></li>
-          <li><button className='btn-nav' onClick={() => onPressCallback('BRACO')}>BRAÇO</button></li>
-          <li><button className='btn-nav' onClick={() => onPressCallback('ABDOMEN')}>ABDÓMEN</button></li>
-          <li><button className='btn-nav' onClick={() => onPressCallback('COSTAS')}>COSTAS</button></li>
-          <li><button className='btn-nav' onClick={() => onPressCallback('PERNA')}>PERNA</button></li>
+        <button className='btn-hamburger' onClick={() => setShowMenu(!showMenu)}>
+          <div className='hamburger-line' />
+          <div className='hamburger-line' />
+          <div className='hamburger-line' />
+        </button>
+        <ul className={`menu ${showMenu ? 'show' : ''}`}>
+          <li><button className='btn-nav' onClick={() => handleOptionClick('OMBRO')}>OMBRO</button></li>
+          <li><button className='btn-nav' onClick={() => handleOptionClick('PEITO')}>PEITO</button></li>
+          <li><button className='btn-nav' onClick={() => handleOptionClick('BRACO')}>BRAÇO</button></li>
+          <li><button className='btn-nav' onClick={() => handleOptionClick('ABDOMEN')}>ABDÓMEN</button></li>
+          <li><button className='btn-nav' onClick={() => handleOptionClick('COSTAS')}>COSTAS</button></li>
+          <li><button className='btn-nav' onClick={() => handleOptionClick('PERNA')}>PERNA</button></li>
         </ul>
       </div>
       
