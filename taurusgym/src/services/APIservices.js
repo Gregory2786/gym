5f223e5eb6fa6c07be25f21f29fcd1c2c5737b57
Gregory2786 <114622096+Gregory2786@users.noreply.github.com>
2024-02-26 18:08:07 -0300

    Update APIservices.js

--- taurusgym/src/services/APIservices.js
+++ taurusgym/src/services/APIservices.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const API_URL = process.env.REACT_APP_API_URL;
 
 export async function getFuncion(musculo) {
-    const response = await axios.get(`${API_URL}/:muscle`, {
-        params: { musculo }
-    });
+    const response = await axios.get(`${API_URL}/${musculo}`);
+
     return response.data;
 }
