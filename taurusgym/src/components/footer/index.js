69cf8ee4f9c8801831a9cf82b9bf8f7a514e7332
JoaoPauloSouzaSilva <joaopaulloss477@gmail.com>
2024-02-26 17:22:19 -0300

    solucionando warning

--- taurusgym/src/components/footer/index.js
+++ taurusgym/src/components/footer/index.js
@@ -9,9 +9,9 @@ const Footer = () => {
       <div className='container-footer'>
         <h4>CONTATOS</h4>
         <ul className='Icones-footer'>
-          <li className='icone'><a href="https://github.com/JoaoPauloSouzaSilva/Taurus-GYM" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a></li>
-          <li className='icone'><a href="https://mail.google.com" target="_blank" rel="noreferrer"><i class="fas fa-envelope"></i></a></li>
-          <li className='icone'><a href="https://www.facebook.com" target="_blank" rel="noreferrer"><i class="fab fa-facebook"></i></a></li>
+          <li className='icone'><a href="https://github.com/JoaoPauloSouzaSilva/Taurus-GYM" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a></li>
+          <li className='icone'><a href="https://mail.google.com" target="_blank" rel="noreferrer"><i className="fas fa-envelope"></i></a></li>
+          <li className='icone'><a href="https://www.facebook.com" target="_blank" rel="noreferrer"><i className="fab fa-facebook"></i></a></li>
         </ul>
       </div>
 
