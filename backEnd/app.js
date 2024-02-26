a1938e15d643996c03f22789a872520a56009991
Everton Sousa Oliveira <105239083+Evertonsousa3@users.noreply.github.com>
2024-02-26 19:04:49 -0300

    update

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,7 +6,10 @@ app.use(express.json())
 require("dotenv").config();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Headers', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
     next();
 });
 const Exercise = mongoose.model('Exercise', { 
